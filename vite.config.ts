4d4d67b2e556607bbbd2d6131516dced7619ec51
an-kulinich <an.kulinich96@gmail.com>
2026-02-15 23:40:36 +0300

    add dev dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import path from "path"
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react-swc"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'an-kulinich/tmbd-kinopoisk',
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@/': `${path.resolve(__dirname, 'src')}/`,
+    },
+  },
 })
