742541803c86093e1c7076a407dfbc890b282756
an-kulinich <an.kulinich96@gmail.com>
2026-08-09 14:15:36 +0300

    fix: fix footer in the bottom of page

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,16 @@
 import { Header } from "@/common/components/header/Header.tsx";
 import { Routing } from "@/common/routing/Routing.tsx";
 import { Footer } from "@/common/components/footer/Footer.tsx";
+import s from "./app.module.css";
 
 export const App = () => {
-
     return (
-        <>
+        <div className={s.app}>
             <Header />
-            <Routing />
+            <main className={s.main}>
+                <Routing />
+            </main>
             <Footer />
-        </>
+        </div>
     );
 };
